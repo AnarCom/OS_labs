34bfa49af94dcd30952de37d4aed202e7613115f
anarcom <alex20021505@gmail.com>
2021-07-17 22:59:00 +0700

    first commit

--- 4lab/list.c
+++ 4lab/list.c
@@ -6,25 +6,4 @@
 Node * fillNode(Node * node, char * string) {
 	node -> data = (char *) calloc(strlen(string) + 1, sizeof(char));
 	if(node -> data == NULL){
-		perror("cannot create node -> data (calloc)");
-		return NULL;
-	}
-	memcpy(node -> data, string, strlen(string) * sizeof(char));
-	return node;
-}
-
-Node * createNote(char * string) {
-	Node * node = malloc(sizeof(Node));
-	if(node == NULL){
-		perror("cannot alloc node (malloc(3C))");
-		return NULL;
-	}
-	node -> next = NULL;
-	node = fillNode(node, string);
-	return node;
-}
-
-void deleteNode (Node * node){
-	free(node -> data);
-	free(node);
-}
+		perror("cannoß≈
